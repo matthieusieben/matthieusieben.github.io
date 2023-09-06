fa68899c5fae0c758770c290e9e8fe1db6455747
Matthieu Sieben <matthieu.sieben@gmail.com>
2023-09-06 12:44:23 +0200

    fix(csp): allow connecting to vercel-insights

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,6 +13,7 @@ export function middleware(request: NextRequest) {
         'default-src': [`'self'`],
         'script-src': [`'self'`, `'nonce-${nonce}'`, `'strict-dynamic'`],
         'style-src': [`'self'`, `'unsafe-inline'`],
+        'connect-src': ['https://vitals.vercel-insights.com'],
         'img-src': [`'self'`, `blob:`, `data:`],
         'font-src': [`'self'`],
         'object-src': [`'none'`],
