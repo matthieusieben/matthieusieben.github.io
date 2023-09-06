9efc466d67de68d1e27f7f6ab7cf5dd72c8a54bd
Matthieu Sieben <matthieu.sieben@gmail.com>
2023-09-06 11:06:58 +0200

    fix(home): minor improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,7 @@
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import { fullName, fullTitle, origin } from '@/constants'
-
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: `${fullName} | ${fullTitle}`,
-  description: fullTitle,
-  metadataBase: new URL(origin),
-  openGraph: {
-    type: 'website',
-    siteName: `${fullName} | ${fullTitle}`,
-    title: fullName,
-    description: fullTitle,
-    url: origin,
-  },
-  twitter: {
-    creator: fullName,
-    site: origin,
-    title: fullName,
-    description: fullTitle,
-  },
-  robots: {
-    index: true,
-    indexifembedded: false,
-  },
-} satisfies Metadata
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,54 @@
 import type { Metadata } from 'next'
 import Image from 'next/image'
 
-import { fullName } from '../constants'
+import pictureImport from '~/picture.jpg'
+import { fullName, fullTitle, origin } from '@/constants'
 
-export const metadata = {} satisfies Metadata
+export const metadata: Metadata = {
+  title: `${fullName} | ${fullTitle}`,
+  description: fullTitle,
+  metadataBase: new URL(origin),
+  openGraph: {
+    type: 'website',
+    siteName: `${fullName} | ${fullTitle}`,
+    title: fullName,
+    description: fullTitle,
+    url: origin,
+  },
+  twitter: {
+    creator: fullName,
+    site: origin,
+    title: fullName,
+    description: fullTitle,
+  },
+  robots: {
+    index: true,
+    indexifembedded: false,
+  },
+}
 
 export default function Home() {
   return (
-    <main className="w-full h-screen relative">
+    <main className="w-full h-screen relative overflow-hidden">
       <Image
-        src="/picture.jpg"
-        className="object-cover w-full h-full select-none"
-        width={1920}
-        height={1080}
+        src={pictureImport}
         alt={fullName}
+        quality={90}
+        layout="fill"
+        objectFit="cover"
+        placeholder="blur"
+        sizes={
+          // Full screen, make sure we pick the larger of the two dimensions
+          `max(100vw, calc(100vh / ${
+            pictureImport.width / pictureImport.height
+          }))`
+        }
+        className="w-full h-full select-none max-md:object-[55%] [@media(max-height:639px)]:object-[55%,25%]"
       />
-      <div className="absolute drop-shadow-md text-slate-100 uppercase top-1/3 left-[10%] max-w-[50%] text-5xl font-medium">
-        {fullName}
+      <div className="absolute top-1/3 left-0 w-[50%] min-w-min max-w-fit flex flex-col items-end pl-8">
+        <span className="drop-shadow-md text-slate-100 uppercase text-4xl md:text-5xl font-medium">
+          {fullName}
+        </span>
       </div>
     </main>
   )
