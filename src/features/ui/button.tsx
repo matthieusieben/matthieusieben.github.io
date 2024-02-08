020059e5a369f7a8dfe9fe78d146c44fca162893
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-02-08 11:39:34 +0100

    feat(button): add outlined

--- src/features/ui/button.tsx
+++ src/features/ui/button.tsx
@@ -1,13 +1,15 @@
-import { forwardRef } from '@/utils/polymorphic-component'
 import { clsx } from '@/utils/clsx'
+import { forwardRef } from '@/utils/polymorphic-component'
 
 type Props = {
+  type?: 'button' | 'submit' | 'reset'
   title?: string
+  outlined?: boolean
 }
 
 export const classes = clsx(
   'inline-flex items-center justify-items-center justify-center',
-  'rounded leading-none p-2.5',
+  'rounded-md leading-none p-2.5',
   'font-sans font-semibold font-medium text-sm text-center',
   'text-gray-500',
   'focus:outline-none focus:ring-4 focus:ring-gray-200',
@@ -15,10 +17,32 @@ export const classes = clsx(
   'dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700'
 )
 
+export const classesOutlined = clsx(
+  'border border-gray-300',
+  'shadow-sm',
+  'bg-white',
+  'dark:bg-gray-800 dark:border-gray-700'
+)
+
 export const Button = forwardRef<Props, 'button'>(
-  ({ as: Component = 'button', className, children, ...props }, ref) => {
+  (
+    {
+      as: Component = 'button',
+      className,
+      children,
+      type = 'button',
+      outlined = false,
+      ...props
+    },
+    ref
+  ) => {
     return (
-      <Component ref={ref} className={clsx(classes, className)} {...props}>
+      <Component
+        ref={ref}
+        className={clsx(classes, className, outlined && classesOutlined)}
+        type={type}
+        {...props}
+      >
         {children == null ? props.title : children}
       </Component>
     )
